01d9c70d138eced2e1d467c1d3cfb5ee9630bd95
Chiyoung Ahn <chiyahn@hotmail.com>
2016-08-24 23:28:47 -0700

    Smooth fixed.

--- src/Smooth.h
+++ src/Smooth.h
@@ -11,15 +11,15 @@ inline arma::mat Smooth (arma::mat* xi_k,
   int n = xi_k->n_rows;
   int M = xi_k->n_cols;
 
-  arma::mat xi_n_t(M, n);
+  arma::mat xi_n_t(M, n, arma::fill::zeros);
   arma::mat xi_k_t = xi_k->t(); // produces a copy of a transpose
-  arma::mat	transition_probs_t = transition_probs->t();
+
   xi_n_t.col(n-1) = xi_k_t.col(n-1);
   for (int k = (n-2); k >= 0; k--)
   {
     xi_n_t.col(k) = xi_k_t.col(k) %
-      (transition_probs_t * (xi_n_t.col(k+1) / (xi_past_t->col(k+1))));
-    xi_n_t.col(k) /= sum(xi_n_t.col(k)); // normalize
+      (*transition_probs * (xi_n_t.col(k+1) / (xi_past_t->col(k+1))));
+    xi_n_t.col(k) /= arma::sum(xi_n_t.col(k)); // normalize
   }
 
   return xi_n_t.t();
